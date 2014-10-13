08c4793c6e5e14c38cff41e0e35360c2c1d645e4
nicolsc <nicolas.lesconnec@gmail.com>
2014-10-13 18:58:54 +0200

    Beginningt o play with mux routing + removed the markdown route + handler + updated deps

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/russross/blackfriday"
+	"github.com/gorilla/mux"
+
+	"fmt"
+	"os"
 )
 
 func main() {
@@ -13,13 +14,52 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	fmt.Printf("Listen to port %s",port)
-    http.ListenAndServe(":"+port, nil)
+
+	r := mux.NewRouter().StrictSlash(true)
+    //r.HandleFunc("/", HomeHandler)
+
+	// Posts collection
+	posts := r.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+
+	// Posts singular
+	post := r.PathPrefix("/posts/{id:[0-9]*}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("GET").HandlerFunc(PostShowHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+    
+    r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+    
+	
+
+	fmt.Printf("Listen to port %s", port)
+	http.ListenAndServe(":"+port, r)
+}
+func HomeHandler(rw http.ResponseWriter, r *http.   Request) {
+	fmt.Fprintln(rw, "Home")
+}
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts index")
 }
 
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-	rw.Write(markdown)
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post update")
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post delete")
+}
+func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "post edit")
 }
